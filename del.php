791fd6f63d9798dcc9a94ce372129f9cbed91a41
Michelangelo <dragonbe+github@gmail.com>
2012-06-23 22:58:24 +0200

    Removed commented depricated code

--- del.php
+++ del.php
@@ -13,31 +13,5 @@ $azureBlob = new AzureBlob($_SESSION['azure_account']['account_name'], $_SESSION
 if ($azureBlob->removeBlob($container, $blobName)) {
     header('Location: /browse.php');
 }
-/*
-require_once 'WindowsAzure/WindowsAzure.php';
-use WindowsAzure\Common\Configuration;
-use WindowsAzure\Blob\BlobSettings;
-use WindowsAzure\Blob\BlobService;
-use WindowsAzure\Common\ServiceException;
-
-$config = new Configuration();
-$config->setProperty(BlobSettings::ACCOUNT_NAME, $_SESSION['azure_account']['account_name']);
-$config->setProperty(BlobSettings::ACCOUNT_KEY, $_SESSION['azure_account']['account_key']);
-$config->setProperty(BlobSettings::URI, $_SESSION['azure_account']['account_uri']);
-
-// Create blob REST proxy.
-$blobRestProxy = BlobService::create($config);
-
-try {
-    // Delete container.
-    $blobRestProxy->deleteBlob($_SESSION['azure_container']['container'], $blob);
-    header('Location: /browse.php');
-}
-catch(ServiceException $e){
-    // Handle exception based on error codes and messages.
-    // Error codes and messages are here: 
-    // http://msdn.microsoft.com/en-us/library/windowsazure/dd179439.aspx
-    $code = $e->getCode();
-    $error_message = $e->getMessage();
-    echo $code.": ".$error_message."<br />";
-}*/
+?>
+<p>Something has gone wrong, <a href="/browse.php">please try again</a>.</p>
