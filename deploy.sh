d434281e7ed19681f8f41c81746caa1e8f8df40a
Michelangelo <dragonbe+github@gmail.com>
2015-10-30 15:27:04 +0100

    AzureBlob: auto-composer deployment
    Moving things around for composer

--- deploy.sh
+++ deploy.sh
@@ -74,6 +74,12 @@ curl -sS https://getcomposer.org/installer | php
 # Deployment
 # ----------
 
+##################################################################################################################################
+# Dependency install
+# ----------
+# Invoke Composer in the deployment directory
+echo Invoking composer install in deployment directory $DEPLOYMENT_TARGET
+php -d extension=php_intl.dll $DEPLOYMENT_TARGET/composer.phar install -v --prefer-dist --no-dev --optimize-autoloader --no-interaction
 
 echo Handling Basic Web Site deployment.
 
@@ -92,13 +98,5 @@ if [[ -n "$POST_DEPLOYMENT_ACTION" ]]; then
   "$POST_DEPLOYMENT_ACTION"
   exitWithMessageOnError "post deployment action failed"
 fi
-
-##################################################################################################################################
-# Dependency install
-# ----------
-
-# Invoke Composer in the deployment directory
-echo Invoking composer install in deployment directory $DEPLOYMENT_TARGET
-php -d extension=php_intl.dll $DEPLOYMENT_TARGET/composer.phar install -v --prefer-dist --no-dev --optimize-autoloader --no-interaction
 
 echo "Finished successfully."
